51ddff5b0859b68fb204bb45ab5660783326b391
Salami Khalil <salamikhalil02@gmail.com>
2023-12-04 14:36:23 +0100

    Basic setup

---
+++ src/context/T3Provider.jsx
@@ -0,0 +1,17 @@
+"use client";
+import { Sepolia } from "@thirdweb-dev/chains";
+
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
+export function T3Provider({ children }) {
+  const activeChain = "ethereum";
+
+  return (
+    <ThirdwebProvider
+      activeChain={Sepolia}
+      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+    >
+      {children}
+    </ThirdwebProvider>
+  );
+}
