55a2210ee53ce57c1ac7d6c639dba66be0d38ef4
Salami Khalil <salamikhalil02@gmail.com>
2024-01-30 21:20:00 +0100

    transaction UI update

--- src/app/transactions/page.jsx
+++ src/app/transactions/page.jsx
@@ -1,5 +1,9 @@
 "use client";
-import { useContract, useContractEvents } from "@thirdweb-dev/react";
+import {
+  useAddress,
+  useContract,
+  useContractEvents,
+} from "@thirdweb-dev/react";
 import Link from "next/link";
 import React from "react";
 
@@ -7,6 +11,8 @@ function Page() {
   const { contract } = useContract(
     "0x8f3582084f1AfBB89ebCEe107339c78B3d8F83F5"
   );
+  const address = useAddress();
+  const userAddr = JSON.stringify(address);
   const { data: event } = useContractEvents(contract, "Sent_Bulk_ETH");
   const { data: allEvents } = useContractEvents(contract);
   const { data: eventWithoutListener } = useContractEvents(
@@ -17,29 +23,42 @@ function Page() {
     }
   );
   return (
-    <section className="container mx-auto px-4">
+    <section className="container mx-auto px-4  overflow-hidden ">
       {allEvents && (
-        <>
-          {allEvents.map((event, index) => (
-            <div key={index} className="grid-cols-2   grid">
-              <div>Sent Bulk Token</div>
-              <div>
-                {event.data.to.map((to, index) => (
-                  <div className="flex flex-col" key={index}>
-                    <div> {to}</div>
-                    {/* <div> {to}</div> <div> {to}</div> <div> {to}</div> */}
-                  </div>
-                ))}
-              </div>
-              <div>{event.data.amount.toString() / 1e18} </div>
-              <Link
-                href={`https://sepolia.etherscan.io/tx/${event?.transaction.transactionHash}`}
+        <div className="flex gap-4 flex-col mt-4">
+          {allEvents
+            .filter((event) => event.data.from === address)
+            .map((event, index) => (
+              <div
+                key={index}
+                className="grid-cols-3    grid border p-2 rounded-xl"
               >
-                View
-              </Link>
-            </div>
-          ))}
-        </>
+                <div>{event.eventName}</div>
+                <div>
+                  {event.data.amount.toString() / 1e18}{" "}
+                  {event.eventName === "Sent_Bulk_Token" ? "Token" : "ETH"}{" "}
+                </div>
+                <Link
+                  href={`https://sepolia.etherscan.io/tx/${event?.transaction.transactionHash}`}
+                >
+                  View Transaction Hash
+                </Link>
+                {/* <div>{address}</div> */}
+                {/* <div>{event.data.from}</div> */}
+
+                <div className="col-span-3  mt-2 ">
+                  <span className="uppercase font-semibold">To address</span>
+                  {event.data.to.map((to, index) => (
+                    <div className="flex flex-col" key={index}>
+                      <div> {to}</div>
+                      {/* <div> {to}</div> <div> {to}</div> <div> {to}</div> */}
+                    </div>
+                  ))}
+                </div>
+                {/* <div>{event.data.amount.toString() / 1e18} </div> */}
+              </div>
+            ))}
+        </div>
       )}
       {allEvents && <div>{JSON.stringify(allEvents[2])}</div>}
     </section>
