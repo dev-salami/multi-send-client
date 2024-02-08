060a1c5b2a3095bf5383e607ed22376df7175785
Salami Khalil <salamikhalil02@gmail.com>
2024-02-08 23:16:36 +0100

    navlink update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,13 @@ function Navbar() {
   return (
     <section>
       <div className="flex justify-between items-center container mx-auto p-4">
-        <div>LOGO</div>
+        <div>MULTI-SEND</div>
         <div className=" hidden sm:flex text-sm uppercase  w-1/2 justify-between">
           <Link href="/">Home</Link>
           <Link href="/send">Send</Link>
-          {/* <Link href="/pricing">Pricing</Link>
-          <Link href="/transactions">History</Link> */}
+
           <button className="uppercase">Pricing</button>
           <Link href="/transactions">History</Link>
-          <button className="uppercase">History</button>
 
           <div></div>
         </div>
