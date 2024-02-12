965d0d5b3a69804f191edd951ef23e410f806ac5
Salami Khalil <salamikhalil02@gmail.com>
2024-02-12 20:06:52 +0100

    added chain validator to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 "use client";
 import React from "react";
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useChain } from "@thirdweb-dev/react";
 import Link from "next/link";
 function Navbar() {
+  const { chainId } = useChain();
+
   return (
     <section>
       <div className="flex justify-between items-center container mx-auto p-4">
@@ -16,7 +18,12 @@ function Navbar() {
 
           <div></div>
         </div>
-        <ConnectWallet />
+        <div className="flex gap-1 flex-col  items-center">
+          <ConnectWallet />
+          {chainId !== 11155111 && (
+            <span className="text-xs text-red-500">Unsupported Chain</span>
+          )}
+        </div>
       </div>
       <hr />
     </section>
